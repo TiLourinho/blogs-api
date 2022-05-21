03bac507ed6ecb7d1b1f1703647943501d3caaef
TiLourinho <lourinho.tiago@gmail.com>
2022-05-21 16:34:19 -0300

    feat: adding user route

--- src/api.js
+++ src/api.js
@@ -2,11 +2,13 @@ const express = require('express');
 const errorMiddleware = require('./database/middlewares/error');
 
 const loginRouter = require('./database/routes/loginRoute');
+const userRouter = require('./database/routes/userRoute');
 
 const app = express();
 app.use(express.json());
 
 app.use(loginRouter);
+app.use(userRouter);
 
 app.use(errorMiddleware);
 
