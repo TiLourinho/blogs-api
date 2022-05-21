489a81d914f443278e675bc37e2ff07710858a17
TiLourinho <lourinho.tiago@gmail.com>
2022-05-20 21:28:43 -0300

    feat: BlogPosts migration

--- src/database/migrations/20220520234747-BlogPosts.js
+++ src/database/migrations/20220520234747-BlogPosts.js
@@ -2,10 +2,45 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    const BlogPost = queryInterface.createTable('BlogPosts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      content: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      userId: {
+        allowNull: false,
+        references: {
+          model: 'Users',
+          key: id
+        },
+        type: Sequelize.INTEGER
+      },
+      published: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
 
+    return BlogPost;
   },
 
-  down: async (queryInterface, Sequelize) => {
+  down: async (queryInterface) => {
+    const BlogPost = queryInterface.dropTable('BlogPosts');
 
+    return BlogPost;
   }
 };
