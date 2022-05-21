adbfa5e83827dba1767f46906f373c9b51448750
TiLourinho <lourinho.tiago@gmail.com>
2022-05-21 16:06:15 -0300

    feat: user service

---
+++ src/database/services/UserService.js
@@ -0,0 +1,21 @@
+const { User } = require('../models');
+const errorHandler = require('../../utils/errorHandler');
+const { STATUS_CONFLICT } = require('../../utils/statusCodes');
+
+const create = async (displayName, email, password, image) => {
+  const checkUser = await User.findOne({
+    where: { email },
+  });
+    
+    if (checkUser) {
+      throw errorHandler(STATUS_CONFLICT, 'User already registered');
+    }
+
+  const user = await User.create({ displayName, email, password, image });
+
+  return user;
+};
+
+module.exports = {
+  create,
+};
