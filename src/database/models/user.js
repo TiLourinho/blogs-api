cdc5565e0096773549bcb7ec87b4be841c194592
TiLourinho <lourinho.tiago@gmail.com>
2022-05-20 22:02:14 -0300

    feat: user model

---
+++ src/database/models/user.js
@@ -0,0 +1,20 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING
+  },
+  {
+    tableName: 'Users',
+    timestamps: false,
+    underscored: true
+  });
+
+  return User;
+};
