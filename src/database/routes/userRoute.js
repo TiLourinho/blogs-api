e20b887d8f674d7a6712a0bf8e9e63b9f7057873
TiLourinho <lourinho.tiago@gmail.com>
2022-05-22 17:57:48 -0300

    feat: endpoint GET /user

--- src/database/routes/userRoute.js
+++ src/database/routes/userRoute.js
@@ -1,8 +1,10 @@
 const userRouter = require('express').Router();
 const validateUser = require('../middlewares/validateUser');
+const authenticateToken = require('../middlewares/authenticateToken');
 
 const UserController = require('../controllers/UserController');
 
 userRouter.post('/user', validateUser, UserController.create);
+userRouter.get('/user', authenticateToken, UserController.getAll);
 
 module.exports = userRouter;
