fc2d34da646e0c4a48946304d7e2485b4cde2e13
TiLourinho <lourinho.tiago@gmail.com>
2022-05-23 11:10:28 -0300

    feat: endpoint GET /categories

--- src/database/routes/categoryRoute.js
+++ src/database/routes/categoryRoute.js
@@ -4,6 +4,7 @@ const authenticateToken = require('../middlewares/authenticateToken');
 
 const CategoryController = require('../controllers/CategoryController');
 
+categoryRouter.get('/categories', authenticateToken, CategoryController.getAll);
 categoryRouter.post('/categories', authenticateToken, validateCategory, CategoryController.create);
 
 module.exports = categoryRouter;
