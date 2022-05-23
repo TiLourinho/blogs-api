ffeddf683d76a82ae4500fcf3d45013dc06ef898
TiLourinho <lourinho.tiago@gmail.com>
2022-05-22 21:15:46 -0300

    feat: adding category route

--- src/api.js
+++ src/api.js
@@ -3,12 +3,14 @@ const errorMiddleware = require('./database/middlewares/error');
 
 const loginRouter = require('./database/routes/loginRoute');
 const userRouter = require('./database/routes/userRoute');
+const categoryRouter = require('./database/routes/categoryRoute');
 
 const app = express();
 app.use(express.json());
 
 app.use(loginRouter);
 app.use(userRouter);
+app.use(categoryRouter);
 
 app.use(errorMiddleware);
 
