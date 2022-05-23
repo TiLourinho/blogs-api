4cf84ba514bf793ef3bf923bbad2597bb089dfc0
TiLourinho <lourinho.tiago@gmail.com>
2022-05-23 18:19:55 -0300

    feat: adding post route

--- src/api.js
+++ src/api.js
@@ -4,6 +4,7 @@ const errorMiddleware = require('./database/middlewares/error');
 const loginRouter = require('./database/routes/loginRoute');
 const userRouter = require('./database/routes/userRoute');
 const categoryRouter = require('./database/routes/categoryRoute');
+const postRouter = require('./database/routes/postRouter');
 
 const app = express();
 app.use(express.json());
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use(loginRouter);
 app.use(userRouter);
 app.use(categoryRouter);
+app.use(postRouter);
 
 app.use(errorMiddleware);
 
