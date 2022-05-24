c64594b33d14988e704372b83d7607300006cbba
TiLourinho <lourinho.tiago@gmail.com>
2022-05-24 10:30:29 -0300

    feat: endpoint GET /post:id

--- src/database/routes/postRouter.js
+++ src/database/routes/postRouter.js
@@ -6,5 +6,6 @@ const PostController = require('../controllers/PostController');
 
 postRouter.post('/post', authenticateToken, validatePost, PostController.create);
 postRouter.get('/post', authenticateToken, PostController.getAll);
+postRouter.get('/post/:id', authenticateToken, PostController.getById);
 
 module.exports = postRouter;
