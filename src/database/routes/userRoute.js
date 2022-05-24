263653d022a7649a40f63a3a93e1cd099aac5560
TiLourinho <lourinho.tiago@gmail.com>
2022-05-24 17:37:52 -0300

    feat: endpoint DELETE /user/me

--- src/database/routes/userRoute.js
+++ src/database/routes/userRoute.js
@@ -7,5 +7,6 @@ const UserController = require('../controllers/UserController');
 userRouter.post('/user', validateUser, UserController.create);
 userRouter.get('/user', authenticateToken, UserController.getAll);
 userRouter.get('/user/:id', authenticateToken, UserController.getById);
+userRouter.delete('/user/me', authenticateToken, UserController.remove);
 
 module.exports = userRouter;
