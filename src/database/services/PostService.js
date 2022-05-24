6bcddb37839d36fdabc31184f56c38e0e39a4b66
TiLourinho <lourinho.tiago@gmail.com>
2022-05-24 10:29:04 -0300

    feat: post method getById in service

--- src/database/services/PostService.js
+++ src/database/services/PostService.js
@@ -1,4 +1,6 @@
 const { BlogPost, User, Category } = require('../models');
+const errorHandler = require('../utils/errorHandler');
+const { STATUS_NOT_FOUND } = require('../utils/statusCodes');
 
 const create = async (title, content, categoryIds, userId) => {
   const post = await BlogPost.create({ title, content, userId });
@@ -18,7 +20,23 @@ const getAll = async () => {
   return posts;
 };
 
+const getById = async (id) => {
+  const post = await BlogPost.findOne({
+    where: { id },
+    include: [
+      { model: User, as: 'user', attributes: { exclude: ['password'] } },
+      { model: Category, as: 'categories', through: { attributes: [] } },
+    ],
+  });
+
+  if (!post) {
+    throw errorHandler(STATUS_NOT_FOUND, 'Post does not exist');
+  }
+  return post;
+};
+
 module.exports = {
   create,
   getAll,
+  getById,
 };
