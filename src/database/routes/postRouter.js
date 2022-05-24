c464da6fbade00e75553ae4d2e3ce831ba68382e
TiLourinho <lourinho.tiago@gmail.com>
2022-05-24 20:28:36 -0300

    feat: endpoint GET /post/search

--- src/database/routes/postRouter.js
+++ src/database/routes/postRouter.js
@@ -6,6 +6,7 @@ const PostController = require('../controllers/PostController');
 
 postRouter.post('/post', authenticateToken, validatePost, PostController.create);
 postRouter.get('/post', authenticateToken, PostController.getAll);
+postRouter.get('/post/search', authenticateToken, PostController.search);
 postRouter.get('/post/:id', authenticateToken, PostController.getById);
 postRouter.put('/post/:id', authenticateToken, validatePost, PostController.update);
 postRouter.delete('/post/:id', authenticateToken, PostController.remove);
